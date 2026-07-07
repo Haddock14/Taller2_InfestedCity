e4a87f2e87df301deff8f1067d19411fa935e2d1
Feralixer <feralixer@gmail.com>
2026-07-07 01:12:50 -0400

    Feat: Implementación lógica de la moneda
    * Se implementa la lógica de destruir el GameObject moneda al colisionar con el usuario.
    * Falta arregla la orientación de la moneda (vertical)

--- Assets/Scripts/Gameplay/Moneda.cs
+++ Assets/Scripts/Gameplay/Moneda.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Moneda : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int valorMoneda = 1;
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(" Moneda Recogida");
+            Destroy(gameObject);
+        }
     }
 }
