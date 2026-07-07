b6b5f88670cd9a9f0b1bad013560c23d80bd4a7b
Feralixer <feralixer@gmail.com>
2026-07-07 01:45:40 -0400

    feat: Implementación grafica de la UI
    * Falta arreglar la suma de monedas e implementar el calculo del puntaje en la UI.

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    [Header("UI de Monedas")]
+    public TextMeshProUGUI textoMonedas;
+    private int monedasRecogidas = 0;
+
+    void awake()
+    {
+        instance = this;
+    }
+    // Método para sumar monedas
+    public void SumarMonedas(int cantidad)
+    {
+        monedasRecogidas += cantidad;
+        textoMonedas.text = "Monedas"+ monedasRecogidas.ToString();
+    }
+
     // Método que llamará el Zombie o el Jugador al perder
     public void ReiniciarJuego()
     {
